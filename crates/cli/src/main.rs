0bd343f85bbbe6f25ad8a4217e6b6950ed3933a9
Diego <96022404+dzfrias@users.noreply.github.com>
2023-07-09 14:11:32 +0200

    feat(cli): add options

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -1,6 +1,10 @@
-use std::{ffi::OsStr, fs::File, io, path::PathBuf};
+use std::{
+    fs::File,
+    io::{self, Write},
+    path::PathBuf,
+};
 
-use anyhow::Result;
+use anyhow::{Context, Result};
 use clap::Parser as ArgParser;
 use clap_stdin::FileOrStdin;
 use decorous_backend::render;
@@ -9,16 +13,44 @@ use decorous_frontend::{Component, Parser};
 #[derive(Debug, ArgParser)]
 #[command(author, version, about, long_about = None)]
 struct Cli {
+    #[arg(value_name = "PATH")]
     input: FileOrStdin,
 
-    /// The name of the output file to generate, or "-" to output to stdout
-    #[arg(short, long, default_value = "./out.js")]
+    /// The name of the output file to generate
+    #[arg(short, long, value_name = "PATH", default_value = "./out.js")]
     out: PathBuf,
+    /// Writes the compiled output to stdout
+    #[arg(long, conflicts_with_all = ["out", "html"])]
+    stdout: bool,
+
+    /// Generates an HTML file along with the compiled JS file. The default is index.html
+    #[arg(long, value_name = "PATH", num_args = 0..=1, require_equals = true, default_missing_value = "index.html", default_value = None)]
+    html: Option<PathBuf>,
 }
 
 fn main() -> Result<()> {
     let args = Cli::parse();
-    let parser = Parser::new(&args.input);
+
+    let component = parse_component(&args.input)?;
+
+    if args.stdout {
+        // Write the file to stdout
+        render(&component, &mut io::stdout()).context("problem rendering component to stdout")?;
+    } else {
+        let mut f = File::create(&args.out)?;
+        render(&component, &mut f).context("problem rendering component to file")?;
+    }
+
+    if let Some(html) = args.html {
+        let mut f = File::create(html)?;
+        write!(f, include_str!("./template.html"), args.out.display())?;
+    }
+
+    Ok(())
+}
+
+fn parse_component(input: &str) -> Result<Component> {
+    let parser = Parser::new(input);
     let (ast, errs) = parser.parse();
     let errors_len = errs.len();
     for err in errs {
@@ -27,12 +59,5 @@ fn main() -> Result<()> {
     if errors_len > 0 {
         anyhow::bail!("\ndecorous failed with {errors_len} errors");
     }
-    let component = Component::new(ast);
-    if args.out.as_os_str() == OsStr::new("-") {
-        render(&component, &mut io::stdout())?;
-    } else {
-        let mut f = File::create(args.out)?;
-        render(&component, &mut f)?;
-    }
-    Ok(())
+    Ok(Component::new(ast))
 }
