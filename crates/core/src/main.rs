2f6069c78cbe2b79242dce831a78a60e06010d31
Diego <96022404+dzfrias@users.noreply.github.com>
2023-08-20 08:29:15 -0700

    fix(cache): bail if not found

--- crates/core/src/main.rs
+++ crates/core/src/main.rs
@@ -73,13 +73,14 @@ fn main() -> Result<()> {
             }
         }
         Command::Cache(args) => {
-            let loc = utils::get_cache_base().context("could get cache base")?;
+            let loc = utils::get_cache_base().context("could not get cache base")?;
+            if !loc.exists() {
+                anyhow::bail!("cache does not exist yet!");
+            }
+
             let size = utils::dir_size(&loc).context("error getting size of dir")?;
 
             if args.clean {
-                if !loc.exists() {
-                    anyhow::bail!("cache does not exist yet!");
-                }
                 fs::remove_dir_all(&loc).context("problem removing cache")?;
                 fs::create_dir(&loc).context("problem re-creating cache dir after clean")?;
                 println!("Cleaned cache! {} bytes saved!", HumanBytes(size));
